1d58a8c62fd376009d425944b1d2f31ba29121fb
Kiran Bishnoi <134170873+kiranbishnoi@users.noreply.github.com>
2026-09-22 13:08:38 +0530

    Add PIC16F877A source code for automatic light control

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <xc.h>
+
+#define _XTAL_FREQ 18000000
+
+#pragma config FOSC = HS
+#pragma config WDTE = OFF
+#pragma config PWRTE = ON
+#pragma config BOREN = ON
+#pragma config LVP = OFF
+#pragma config CPD = OFF
+#pragma config WRT = OFF
+#pragma config CP = OFF
+
+void main(void)
+{
+    TRISB = 0x03;       // RB0, RB1 input
+    TRISD = 0x00;       // PORTD output
+
+    OPTION_REGbits.nRBPU = 0;   // Enable PORTB internal pull-ups
+
+    PORTD = 0x00;
+
+    while(1)
+    {
+        if(RB0 == 0)
+        {
+            RD0 = 1;    // Relay ON
+        }
+        else
+        {
+            RD0 = 0;    // Relay OFF
+        }
+    }
+}
